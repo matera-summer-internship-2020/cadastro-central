a429a23e882ac0606fe7fbec193012730d89531f
Francisco <franciscogupe@gmail.com>
2020-12-28 14:49:15 -0300

    First Commit

---
+++ src/main/java/com/matera/cadastrocentral/CadastroCentralApplication.java
@@ -0,0 +1,13 @@
+package com.matera.cadastrocentral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroCentralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroCentralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matera/cadastrocentral/CadastroCentralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matera.cadastrocentral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroCentralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

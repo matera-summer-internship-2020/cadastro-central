917d4ed3761642cd86de29e38646584f081cbdb7
Gabriel Oswaldo <gabriel.oswaldo99@gmail.com>
2021-01-04 15:44:39 -0300

    feat: add GET all clients request

--- src/main/java/com/matera/cadastrocentral/client/ClientController.java
+++ src/main/java/com/matera/cadastrocentral/client/ClientController.java
@@ -1,8 +1,11 @@
 package com.matera.cadastrocentral.client;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/clients")
@@ -15,5 +18,11 @@ public class ClientController {
         this.clientService = clientService;
     }
 
-    //TODO implement the client API requests
+    /* API requests */
+
+    // 1. Get all clients from the database.
+    @GetMapping
+    public List<Client> getAllClients(){
+        return clientService.getAllClients();
+    }
 }
--- src/main/java/com/matera/cadastrocentral/client/ClientService.java
+++ src/main/java/com/matera/cadastrocentral/client/ClientService.java
@@ -3,6 +3,8 @@ package com.matera.cadastrocentral.client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ClientService {
 
@@ -13,5 +15,10 @@ public class ClientService {
         this.clientRepository = clientRepository;
     }
 
-    //TODO implement the client API requests
+    /* API requests */
+
+    // 1. Get all clients from the database.
+    public List<Client> getAllClients() {
+        return clientRepository.findAll();
+    }
 }

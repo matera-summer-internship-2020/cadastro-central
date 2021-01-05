847f6cf15502def2cf2606c7c2b8f9b07e88dd4e
Gabriel Oswaldo <gabriel.oswaldo99@gmail.com>
2021-01-05 08:43:03 -0300

    feat: add exception handling at getClientById

--- src/main/java/com/matera/cadastrocentral/client/ClientService.java
+++ src/main/java/com/matera/cadastrocentral/client/ClientService.java
@@ -1,8 +1,11 @@
 package com.matera.cadastrocentral.client;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -26,6 +29,10 @@ public class ClientService {
 
     // 2. Get a specific client by id.
     public Optional<Client> getClientById(UUID clientId) {
-        return clientRepository.findById(clientId);
+        try {
+            return clientRepository.findById(clientId);
+        } catch (EntityNotFoundException ex) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
     }
 }
